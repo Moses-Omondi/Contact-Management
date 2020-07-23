33367ca5b379b5c4c4c5aa3a6565407d26e9fdce
Moses-Omondi <mose.omondi@gmail.com>
2020-07-23 22:11:23 +0530

    Final app development

--- routes/users.js
+++ routes/users.js
@@ -33,41 +33,41 @@ router.post(
       // Checking if the user exists
       let user = await User.findOne({ email });
 
-      if (user) {
+      if (!user) {
+        // User doesn't exist create an instance
+        user = new User({
+          name,
+          email,
+          password,
+        });
+
+        // Encrypt the password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        // Creating a token
+        const payload = {
+          user: {
+            id: user.id,
+          },
+        };
+
+        jwt.sign(
+          payload,
+          config.get("jwtSecret"),
+          {
+            expiresIn: 3600,
+          },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          }
+        );
+      } else {
         res.status(400).json({ msg: "User already exists" });
       }
-
-      // User doesn't exist create an instance
-      user = new User({
-        name,
-        email,
-        password,
-      });
-
-      // Encrypt the password
-      const salt = await bcrypt.genSalt(10);
-      user.password = await bcrypt.hash(password, salt);
-
-      await user.save();
-
-      // Creating a token
-      const payload = {
-        user: {
-          id: user.id,
-        },
-      };
-
-      jwt.sign(
-        payload,
-        config.get("jwtSecret"),
-        {
-          expiresIn: 3600,
-        },
-        (err, token) => {
-          if (err) throw err;
-          res.json({ token });
-        }
-      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server Error");
--- server.js
+++ server.js
@@ -1,20 +1,49 @@
-const express = require('express');
-const connectDB = require('./config/db')
+const express = require("express");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
 
 const app = express();
 
 // Connect Database
 connectDB();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-// Define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  
+  if(req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
+})
 
+// Define Routes
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`)); 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
