5fa1f82facf94d2bd2bbdc5018ee1d92a429e2d2
Moses-Omondi <mose.omondi@gmail.com>
2020-07-23 22:59:09 +0530

    Prepare for deploy

---
+++ client/build/precache-manifest.ea7fdd74fb1e5dacd98c479a4e74cb4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa26220c388a4661c0f7d94a9ed412a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0dd1a9c6528bd4fc3f9d",
+    "url": "/static/css/main.a98cff5b.chunk.css"
+  },
+  {
+    "revision": "457a278df7b40818b7b1",
+    "url": "/static/js/2.cbe98f8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cbe98f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dd1a9c6528bd4fc3f9d",
+    "url": "/static/js/main.5dd1d5fd.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -14,20 +15,30 @@ app.use(bodyParser.json());
 app.use(express.json({ extended: false }));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  
-  if(req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).json({});
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
   }
   next();
-})
+});
 
 // Define Routes
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.use((req, res, next) => {
   const error = new Error("Not Found");
